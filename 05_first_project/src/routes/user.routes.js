2d908bd0f5228ea38bf4f148a9732a28d8b5e079
Nitish <deshmukhnitish0@gmail.com>
2024-08-04 22:34:41 +0530

    fix all regiser controller bugs

--- 05_first_project/src/routes/user.routes.js
+++ 05_first_project/src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {registerUser} from "../controllers/user.controller.js";
-import {upload} from "../middleweares/multer.middleweare.js";
+import {upload} from "../middlewares/multer.middleware.js";
+
 
 const router = Router()
 
@@ -17,5 +18,6 @@ router.route("/register").post(
     ]),
     registerUser
 )
+
 
 export default router
--- 05_first_project/src/utils/cloudinary.service.js
+++ 05_first_project/src/utils/cloudinary.service.js
@@ -1,5 +1,5 @@
 import { v2 as cloudinary } from 'cloudinary';
-
+import fs from "fs"
 
 cloudinary.config({ 
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -15,7 +15,8 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: "auto"
         })
         // file has been uploaded sccessfull
-        console.log("file is uploaded on cloudinary", response.url);
+        // console.log("file is uploaded on cloudinary", response.url);
+        fs.unlinkSync(localFilePath)
         return response;
     } catch (error) {
         fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
